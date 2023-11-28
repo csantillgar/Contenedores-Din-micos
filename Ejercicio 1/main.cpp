bdfdf5a7366c7c80be10b1c137ec1054de4e4a20
csantillgar <csantill@myuax.com>
2023-11-28 10:50:00 +0100

    commit 1

---
+++ Ejercicio 1/main.cpp
@@ -0,0 +1,4 @@
+//Crea un programa que añada elementos a un vector de enteros mediante el uso del método push_back. Luego, utiliza un iterador para recorrer y mostrar cada elemento del vector.
+#include <iostream>
+#include <vector>
+using namespace std;
