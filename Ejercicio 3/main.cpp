d9f320fdb60ff45a5e7b3ef7c1be45598e936a41
csantillgar <csantill@myuax.com>
2023-11-30 09:55:09 +0100

    ejercicio3

---
+++ Ejercicio 3/main.cpp
@@ -0,0 +1,9 @@
+//Ejercicio 3: Algoritmos Crea un programa que tenga un vector de enteros. Luego, utiliza la función sort de la biblioteca de algoritmos para ordenar los elementos del vector. Además, utiliza la función max_element para encontrar el elemento más grande del vector.
+#include <iostream>
+#include <vector>
+#include <algorithm>
+int main(){
+
+
+    return 0;
+}
