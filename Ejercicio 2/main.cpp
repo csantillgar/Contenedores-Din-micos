a7a27abf2f9c719475455f53ee6d7c0df40577ff
csantillgar <csantill@myuax.com>
2023-11-30 09:15:27 +0100

    ejercicio2

---
+++ Ejercicio 2/main.cpp
@@ -0,0 +1,9 @@
+//Ejercicio 2: Contenedores Estándar y Secuencias Realiza un programa que utilice el contenedor estándar list para almacenar una secuencia de números decimales. Luego, crea un adaptador de secuencia stack para este contenedor. Muestra cómo funcionan las operaciones push, pop y top en esta pila.
+#include<iostream>
+#include<list>
+#include<stack>
+using namespace std;
+int main{
+
+    return 0;
+};
