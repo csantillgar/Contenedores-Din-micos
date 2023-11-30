86b0267317b897a161b50b80bec91b5653b2e773
csantillgar <csantill@myuax.com>
2023-11-30 09:07:07 +0100

    ejercicio1

--- Ejercicio 1/main.cpp
+++ Ejercicio 1/main.cpp
@@ -2,3 +2,12 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+int main() {
+    vector<int> numeros = {10, 20, 30, 40, 50};
+
+    for(vector<int>::iterator it = numeros.begin(); it != numeros.end(); ++it) {
+        std::cout << *it << std::endl;
+    }
+
+    return 0;
+}
