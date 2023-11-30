e7277edd3962fc43ad3d9118380bb37f4bab4f12
csantillgar <csantill@myuax.com>
2023-11-30 10:06:17 +0100

    Ejercicio4

---
+++ Ejercicio 4/main.cpp
@@ -0,0 +1,7 @@
+//Ejercicio 4: Cálculo numérico Desarrolla un programa que utilice la clase valarray para realizar operaciones matemáticas. Crea un valarray de números decimales e implementa operaciones como la suma, la resta, la multiplicación y la división.
+#include<iostream>
+int main(){
+
+
+    return 0 ;
+}
